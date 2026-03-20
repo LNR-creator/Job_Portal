395a1802ea79a64f4b752c2c72bf3fe8677796f9
Pasula Laxmi Narasimha Raju <lnraju366@gmail.com>
2026-03-20 12:51:47 +0530

    Switch to sqlite for deployment

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -81,15 +81,8 @@ WSGI_APPLICATION = "job_portal.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DB_NAME", "job__portal"),
-        "USER": os.environ.get("DB_USER", "root"),
-        "PASSWORD": os.environ.get("DB_PASSWORD", "rajucoder10624"),
-       "HOST": os.environ.get("DB_HOST", "localhost"),
-        "PORT": os.environ.get("DB_PORT", "3306"),
-        "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
